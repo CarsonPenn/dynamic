80bdecbaa1af2f644178170a8108e1c164d210f1
CarsonPenn <carsonpenn@byui.edu>
2024-11-13 17:23:54 -0700

    end of class day commits

---
+++ tatooine/templates/inclass.js
@@ -0,0 +1,11 @@
+const form = document.querySelector(".login-form")
+const username = document.querySelector(".username")
+const password = document.querySelector(".password")
+
+function onLogin(event){
+    event.preventDefault();
+
+    if (username.value)
+}
+
+form.addEventListener("submit, onLogin");
